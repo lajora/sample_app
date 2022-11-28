0b62ebf7cd473b92bde34eac0e3917b4ddcc0457
lajora <lara.jr.keen@gmail.com>
2022-11-28 16:19:50 +0100

    relationship validation

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -23,7 +23,7 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
     log_in_as(users(:michael))
     micropost = microposts(:ants)
     assert_no_difference 'Micropost.count' do
-      delete micropost_path(micropost)
+      sdelete micropost_path(micropost)
     end
     assert_redirected_to root_url
   end
